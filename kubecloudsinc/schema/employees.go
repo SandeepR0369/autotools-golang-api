ec672442123b83933f287e77706f89dfbd453865
sandeepr0963 <sandeep.bsr007@gmail.com>
2024-03-03 02:11:59 -0500

    Added README.MD

--- kubecloudsinc/schema/employees.go
+++ kubecloudsinc/schema/employees.go
@@ -1,22 +1,5 @@
 package schema
 
-// "database/sql"
-// Include other imports as needed
-
-// type Employee struct {
-// 	EmployeeID    int     `json:"employeeId"`
-// 	FirstName     string  `json:"firstName"`
-// 	LastName      string  `json:"lastName"`
-// 	Email         string  `json:"email"`
-// 	Phone         string  `json:"phone"`
-// 	HireDate      string  `json:"hireDate"`
-// 	JobID         string  `json:"jobId"`
-// 	Salary        float64 `json:"salary"`
-// 	CommissionPct any     `json:"commissionPct"`
-// 	ManagerID     sql.NullInt64     `json:"managerId"`
-// 	DepartmentID  sql.NullInt64     `json:"departmentId"`
-// }
-
 type Employee struct {
 	EmployeeId    *int     `json:"employeeId"`
 	FirstName     *string  `json:"firstName"`
